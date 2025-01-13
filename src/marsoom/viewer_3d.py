4ff4dad0a42b36e770fdf21c3eb42e2f32255a05
Jad Abou-Chakra <29726242+jc211@users.noreply.github.com>
2025-01-13 10:50:06 -0500

    fix

--- src/marsoom/viewer_3d.py
+++ src/marsoom/viewer_3d.py
@@ -461,6 +461,7 @@ class Viewer3D:
     def x_vw(self, standard: Literal['opencv', 'blender'] = 'blender') -> np.ndarray:
         x_vw = self._view_matrix.reshape((4, 4)).T
         if standard == 'opencv':
+            x_vw = x_vw.copy()
             x_vw[1] = -x_vw[1]
             x_vw[2] = -x_vw[2]
         return x_vw

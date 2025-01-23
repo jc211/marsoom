9694815439c4d937792ebec33152921821d42b70
Jad Abou-Chakra <29726242+jc211@users.noreply.github.com>
2025-01-23 16:19:27 -0500

    add import

--- src/marsoom/cuda/__init__.py
+++ src/marsoom/cuda/__init__.py
@@ -2,3 +2,4 @@ from .point_renderer import PointRenderer
 from .mesh_renderer import InstancedMeshRenderer
 from .ellipse_renderer import EllipseRenderer
 from .vector_renderer import VectorRenderer
+from .render_opengl import OpenGLRendererWrapper, OpenGLRenderer

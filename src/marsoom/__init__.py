4541940d0ca43936d025ce92c841ef426f1bfbac
Jad Abou-Chakra <29726242+jc211@users.noreply.github.com>
2025-03-11 19:49:22 -0400

    pin pyglet and remove shadow_window because of locale problems

--- src/marsoom/__init__.py
+++ src/marsoom/__init__.py
@@ -1,6 +1,11 @@
-from marsoom.viewer_3d import Viewer3D
-from marsoom.viewer_2d import Viewer2D, compute_affine_transform, eViewerUnit
+import pyglet
 from marsoom.window import Window
+
+pyglet.options["shadow_window"] = False
+pyglet.window.xlib._have_utf8 = False
+from marsoom.viewer_3d import Viewer3D
+
+from marsoom.viewer_2d import Viewer2D, compute_affine_transform, eViewerUnit
 from marsoom.camera_wireframe import CameraWireframe, CameraWireframeWithImage
 from marsoom.axes import Axes
 from marsoom.utils import convert_K_to_projection_matrixT, COLORS
@@ -11,4 +16,6 @@ from marsoom.grid import Grid
 from marsoom.texture import Texture
 from marsoom.overlay import Overlay
 from imgui_bundle import imguizmo, imgui
+
 guizmo = imguizmo.im_guizmo
+

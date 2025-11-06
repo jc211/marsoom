57f303c07e3442368a1e749e0ffdc4751090b812
jc211 <29726242+jc211@users.noreply.github.com>
2025-11-06 10:55:18 +1000

    remove linux specific things in import

--- src/marsoom/__init__.py
+++ src/marsoom/__init__.py
@@ -1,21 +1,20 @@
 import pyglet
+from imgui_bundle import imgui, imguizmo
+
+from marsoom.axes import Axes
+from marsoom.camera_wireframe import CameraWireframe, CameraWireframeWithImage
+from marsoom.grid import Grid
+from marsoom.overlay import Overlay
+from marsoom.points import Points
+from marsoom.shape_3d import Circle, Point
+from marsoom.structured_pointcloud import StructuredPointCloud
+from marsoom.texture import Texture
+from marsoom.utils import COLORS, convert_K_to_projection_matrixT
+from marsoom.viewer_2d import Viewer2D, compute_affine_transform, eViewerUnit
+
+# pyglet.options["shadow_window"] = False
+# pyglet.window.xlib._have_utf8 = False
+from marsoom.viewer_3d import Viewer3D
 from marsoom.window import Window
 
-pyglet.options["shadow_window"] = False
-pyglet.window.xlib._have_utf8 = False
-from marsoom.viewer_3d import Viewer3D
-
-from marsoom.viewer_2d import Viewer2D, compute_affine_transform, eViewerUnit
-from marsoom.camera_wireframe import CameraWireframe, CameraWireframeWithImage
-from marsoom.axes import Axes
-from marsoom.utils import convert_K_to_projection_matrixT, COLORS
-from marsoom.shape_3d import Circle, Point
-from marsoom.points import Points
-from marsoom.structured_pointcloud import StructuredPointCloud
-from marsoom.grid import Grid
-from marsoom.texture import Texture
-from marsoom.overlay import Overlay
-from imgui_bundle import imguizmo, imgui
-
 guizmo = imguizmo.im_guizmo
-
